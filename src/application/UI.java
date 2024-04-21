f40f8791430ef2f60a4269fae9d9aed07403cc95
michelesantosl1 <misanttos300@gmail.com>
2024-04-21 12:12:04 -0300

    Knight

--- src/application/UI.java
+++ src/application/UI.java
@@ -56,12 +56,12 @@ public class UI {
 		printCapturedPieces(captured);
 		System.out.println();
 		System.out.println("Turn : " + chessMatch.getTurn());
-		if (!chessMatch.getCheck()) {
+		if (!chessMatch.getCheckMate()) {
 			System.out.println("Waiting player: " + chessMatch.getcurrentPlayer());
 			if (chessMatch.getCheck()) {
 				System.out.println("CHECK!");
 			}
-		} 
+		}
 		else {
 			System.out.println("CHECKMATE!");
 			System.out.println("Winner: " + chessMatch.getcurrentPlayer());
